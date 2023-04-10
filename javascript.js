dfdb5126673b28960ebe0b80f42b161ee5868e36
kawanro <32939470+kawanro@users.noreply.github.com>
2023-04-10 15:58:06 +0200

    added random colors

--- javascript.js
+++ javascript.js
@@ -1,15 +1,27 @@
 const container = document.getElementById("gridContainer");
 
 function addColor(){
-    //this.setAttribute('style', "background: blue;")
-    this.classList.add("blue");
+    let red = randomColor();
+    let green = randomColor();
+    let blue = randomColor();
+    //this.classList.add("blue");
+    //this.style.background = "rgb(" + green + "," +red+","+blue
+    //this.style.background = "hsl(" + green + ",100%,50%"
+    if(this.style.background == ""){
+        this.style.background = "hsl(" + green + ",100%,50%"
+    }
+
+    
+}
+
+function randomColor(){
+    let x = Math.floor(Math.random()*360);
+    return x;
 }
 
 
 function createSquares(num){
-    console.log(container);
     const height = 100/num;
-    console.log(height);
 
     for (i=0;i<num;i++)
     {
@@ -32,10 +44,25 @@ function createSquares(num){
 
     const allSquares = document.querySelectorAll(".grid");
 
-    allSquares.forEach(square => square.addEventListener('mouseover', addColor));
+    allSquares.forEach(square => square.addEventListener('mouseout', addColor));
     
     
 
 }
 
-createSquares(32);
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+    let userInput = prompt("Please enter the size of your grid ");
+    userInput = parseInt(userInput);
+    if (userInput="NaN" || userInput<1){
+        alert("Please enter a number over 0");
+        userInput = prompt("Please enter the size of your grid ");
+    }
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    createSquares(userInput);
+});
+
+createSquares(16);
